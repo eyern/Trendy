9801747f96b6fda54669c2989e715db3dce1f04a
eyern <eyern254@gmail.com>
2024-11-11 14:54:59 +0300

    overhaul

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,260 @@
-console.log("https://github.com/serdargoleli/shoes-ecommerce-page");
+/**
+* Template Name: Dewi
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/dewi-free-multi-purpose-html-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 
-/* ===========> SEARCH BOX <===========  */
-const btnSearchBoxToggle = document.getElementById("btn-toggle-search-box");
-const searchBoxOverlay = document.getElementById("search-box-overlay");
-const searchBoxContainer = document.getElementById("search-box-container");
-const searchInput = document.getElementById("search-input");
+(function() {
+  "use strict";
 
-btnSearchBoxToggle.addEventListener("click", () => {
-  searchBoxOverlay.classList.toggle("open-search-box-overlay");
-  searchBoxContainer.classList.toggle("open-search-box");
-  /* Eger search-box açıksa inputa focusla ve içindeki değeri sıfırla  */
-  if (searchBoxContainer.classList.contains("open-search-box")) {
-    searchInput.focus();
-    searchInput.value = "";
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
   }
-});
 
-searchBoxOverlay.addEventListener("click", () => {
-  if (searchBoxContainer.classList.contains("open-search-box")) {
-    searchBoxOverlay.classList.remove("open-search-box-overlay");
-    searchBoxContainer.classList.remove("open-search-box");
-    console.log("...");
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
   }
-});
 
-/* ===========> MOBILE MENU <===========  */
-/* Menu Show */
-const btnToggleMobileMenu = document.getElementById("btn-toggle-mobile-menu");
-const overlay = document.getElementById("mobile-menu-overlay");
-const mobileMenuBox = document.getElementById("mobile-menu-box");
-const btnCloseMenu = document.getElementById("btn-close-menu");
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
 
-btnToggleMobileMenu.addEventListener("click", () => {
-  overlay.classList.toggle("open-overlay");
-  mobileMenuBox.classList.toggle("open-mobile-menu");
-});
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
 
-/* Menu Close */
-overlay.addEventListener("click", () => {
-  overlay.classList.remove("open-overlay");
-  mobileMenuBox.classList.remove("open-mobile-menu");
-});
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
 
-btnCloseMenu.addEventListener("click", () => {
-  mobileMenuBox.classList.remove("open-mobile-menu");
-  overlay.classList.remove("open-overlay");
-});
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 20
+    }
 
-// Home Select Shoes Size
-var div = document.getElementById("select-size");
-var items = div.getElementsByClassName("size-item");
-for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("checked");
-    current[0].className = current[0].className.replace(" checked", "");
-    this.className += " checked";
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
   });
-}
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+
+
+  function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+
+    let portfolioContainer = select('.portfolio-container');
+
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
